cbacd4b610191363cb6fcaac7b76ee2b02fa0948
Ed Prince <edward_prince@hotmail.com>
2016-01-19 13:52:12 +0000

    travis fix

--- test_sort.py
+++ test_sort.py
@@ -5,7 +5,7 @@ def bubble_sort(x):
         for i in range(len(x) - 1):
             if x[i] < x[i + 1]:
                 tmp = x[i + 1]
-                x[i - 1] = x[i]
+                x[i + 1] = x[i]
                 x[i] = tmp
             i += 1
         i += 1
