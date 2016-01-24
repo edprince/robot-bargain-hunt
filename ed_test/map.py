5b9581a2e07accb076a1b556464481943e475fbf
Ed Prince <edward_prince@hotmail.com>
2016-01-24 03:16:37 +0000

    working on random tree generation;

--- ed_test/map.py
+++ ed_test/map.py
@@ -1,6 +1,12 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
+
+#Constants#
+MAPHEIGHT = 16
+TILESIZE = 32
+MAPWIDTH = 26
 
 DIRT = 0
 GRASS = 1
@@ -8,6 +14,8 @@ STONE = 3
 WATER = 4
 SAND = 5
 WOOD = 6
+
+TREE_DENSITY = 10
 
 file_object = open('map_data.txt', 'r')
 map_data = file_object.read()
@@ -32,10 +40,6 @@ for line in map_data:
         tilemap_tmp = []
 
 
-BROWN = (153, 76, 0)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 
 colors = {
@@ -46,15 +50,36 @@ colors = {
         SAND: pygame.image.load('assets/sand.png'),
         WOOD: pygame.image.load('assets/wood.png')
         }
-TILESIZE = 32
-MAPWIDTH = 26
-MAPHEIGHT = 16
 
 playerPos = [0,0]
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
 PLAYER = pygame.image.load('assets/player.png').convert_alpha()
+TREE = pygame.image.load('assets/tree.png').convert_alpha()
+DISPLAYED_TREES = False
+TREE_LOCATIONS = []
+
+for i in range(MAPHEIGHT):
+    for j in range(MAPWIDTH):
+        if tilemap[i][j] == 1:
+            DISPLAYSURF.blit(TREE, (2, 0)) 
+            if random.randrange(100) > TREE_DENSITY:
+                #DISPLAYSURF.blit(TREE, (0, 0))#
+                pass
+
+for row in range(MAPHEIGHT):
+    for column in range(MAPWIDTH):
+        if tilemap[row][column] != 1:
+            print "boo"
+            if random.randrange(100) < TREE_DENSITY:
+                #TREE_LOCATIONS.append([row, column])#
+                DISPLAYSURF.blit(TREE, (row*TILESIZE, column*TILESIZE))
+pygame.display.update()
+
+
+            
+
 
 while True:
     for event in pygame.event.get():
@@ -66,10 +91,13 @@ while True:
                 playerPos[0] += 1
             elif (event.key == K_LEFT) and ((playerPos[0] - 1) >= 0):
                 playerPos[0] -= 1
-            elif (event.key == K_DOWN) and ((playerPos[1] + 1) < MAPHEIGHT):
+            elif (event.key == K_DOWN) and ((playerPos[1] + 1) < MAPHEIGHT and
+                    tilemap[playerPos[0]][playerPos[1] + 1] != 4):
                 playerPos[1] += 1
             elif (event.key == K_UP) and ((playerPos[1] - 1) >= 0):
                 playerPos[1] -= 1
+
+            print tilemap
 
         for row in range(MAPHEIGHT):
             for column in range(MAPWIDTH):
@@ -77,5 +105,10 @@ while True:
                         (column*TILESIZE, row*TILESIZE))
                 DISPLAYSURF.blit(PLAYER, (playerPos[0]*TILESIZE,
                     playerPos[1]*TILESIZE))
+                '''
+                if [row, column] in TREE_LOCATIONS:
+                    DISPLAYSURF.blit(TREE, (row*TILESIZE, column*TILESIZE))
+                    '''
+
 
         pygame.display.update()
