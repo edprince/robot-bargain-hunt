cc7c2457c5c524254deeacf5fdbe4acd11204fcd
Ed Prince <edward_prince@hotmail.com>
2016-02-09 10:24:47 +0000

    added distance calculation function

--- functions.py
+++ functions.py
@@ -11,3 +11,12 @@ def sort_objects(x):
                 x[i + 1] = x[i]
                 x[i] = tmp
     return x
+
+def calculate_distance(x1, y1, x2, y2):
+    '''Calculate distance between two coordinates
+
+    Takes 2 sets of coordinates, and calculates the distance between using
+    Pythagoras's theorem'''
+    width = math.sqrt((x2 - x1)**2)
+    height = math.sqrt((y2 - y1)**2)
+    return math.sqrt(width**2 + height**2)
