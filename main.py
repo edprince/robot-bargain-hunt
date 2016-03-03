90f62a99b15cc85c8dcd75ccfeea2e3864fbf09e
Ed Prince <edward_prince@hotmail.com>
2016-03-03 01:03:54 +0000

    Added line that runs function in main automatically (just for testing), and some assets for the map

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import sys
 import math
 import random
 
+#pygame dependent function
+def sort_highlight(pX, pY, oX, oY):
+    '''Function to highlight items being sorted
+
+    Takes 4 integers, position of item, and position of player'''
+    pygame.draw.line(DISPLAYSURF, BLUE, (pX, pY), (oX, oY), 1)
+    
+
 def start():
     objs = []
     #time_input = int(raw_input("Enter a time: "))
@@ -15,9 +23,12 @@ def start():
     WIDTH = 50
     HEIGHT = 30
     TILESIZE = 32
+    ROCK_DENSITY = 10
     BLUE = (0, 0, 255)
     TIME = 50000
-    SAND = pygame.image.load('assets/sand.png')
+    SAND = pygame.image.load('assets/sand-new.png')
+    STONE = pygame.image.load('assets/stone.png')
+    BUSH = pygame.image.load('assets/bush.png')
 
     #Initialize items
     item_1 = pygame.image.load('assets/crown.png')
@@ -40,12 +51,15 @@ def start():
     for i in range(len(game_items)):
         objs.append(game_items[items[i]])
 
+    print("Sorting into order")
     if ASCENDING == True:
         sorted_list = sort_objects(objs)
     else:
         sorted_list = sort_objects(objs)[::-1]
 
     pygame.init()
+    rock_locations = []
+    bush_locations = []
     #Initialize pygame dependent variables
     flags = DOUBLEBUF
     DISPLAYSURF = pygame.display.set_mode((WIDTH*TILESIZE, HEIGHT*TILESIZE + 40), flags)
@@ -54,8 +68,19 @@ def start():
     player = pygame.image.load('assets/player-idea.png')
     pygame.time.set_timer(USEREVENT, TIME)
 
-    #place player in the middle of the screen
+    for rows in range(WIDTH):
+        for columns in range(HEIGHT):
+            if random.randrange(1000) < ROCK_DENSITY:
+                rock_locations.append([rows, columns])
+            elif (random.randrange(1000) > 990):
+                bush_locations.append([rows, columns])
+
+
+
+
     playerPos = [WIDTH / 2, HEIGHT / 2]
+
+    #place player in the middle of the screen
     first_it = True
     game_running = True
     searchingFor = 1
@@ -102,10 +127,24 @@ def start():
             for column in range(WIDTH):
                 DISPLAYSURF.blit(SAND, (column * TILESIZE, row * TILESIZE))
                 DISPLAYSURF.blit(player, (playerPos[0] * TILESIZE,(playerPos[1]) * TILESIZE))
+                if [column, row] in rock_locations:
+                    DISPLAYSURF.blit(STONE, (column * TILESIZE, row * TILESIZE))
+                if [column, row] in bush_locations:
+                    DISPLAYSURF.blit(BUSH, (column * TILESIZE, row * TILESIZE))
+
         
         for i in game_items:
             if (game_items[i].hidden != True):
                 DISPLAYSURF.blit(i, (game_items[i].location[0] * TILESIZE, game_items[i].location[1] * TILESIZE))
+
+        for i in game_items:
+            if game_items[i].hidden != True:
+                pygame.draw.line(DISPLAYSURF, BLUE, (playerPos[0] * TILESIZE,
+                    playerPos[1] * TILESIZE), (game_items[i].location[0] *
+                        TILESIZE, game_items[i].location[1] * TILESIZE), 2)
+            #print(game_items[i].location[0])
     
         pygame.display.update()
         clock.tick(60)
+
+start()
